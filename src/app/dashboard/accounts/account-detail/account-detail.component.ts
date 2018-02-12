463d3fce2667c6eb5c13078de98865f7d7bacdb6
Javier Delgado <dn.javier@hotmail.com>
2018-02-12 11:02:57 -0600

    adds account details markup and data from the component

--- src/app/dashboard/accounts/account-detail/account-detail.component.ts
+++ src/app/dashboard/accounts/account-detail/account-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ITransaction } from '../models/transaction.model';
+import { IAccount } from '../models/account.model';
 
 @Component({
   selector: 'ds-account-list',
@@ -7,6 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./account-detail.component.scss']
 })
 export class AccountDetailComponent implements OnInit {
+  
+  transactions: ITransaction[] = [
+    {
+      date: '9 FEB 18',
+      accFrom: 1,
+      accTo: 2,
+      amount: 5000.00,
+      description: 'GIC principal cancelled from 101213153'
+    },
+    {
+      date: '1 FEB 18',
+      accFrom: 1,
+      accTo: 2,
+      amount: 345.00,
+      description: 'GIC principal cancelled from 101213154'
+    }
+  ];
+
+  account: IAccount = {
+    name: 'Daily savings',
+    number: 1,
+    user: 1,
+    amount: 500
+  };
   
   constructor(){ }
 
