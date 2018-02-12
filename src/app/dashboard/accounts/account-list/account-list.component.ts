c9c829b2a5e563cde42fa07d846b18f6fb31838e
Javier Delgado <dn.javier@hotmail.com>
2018-02-12 09:32:14 -0600

    adds a model for transactions

--- src/app/dashboard/accounts/account-list/account-list.component.ts
+++ src/app/dashboard/accounts/account-list/account-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ITransaction } from '../models/transaction.model';
 
 @Component({
   selector: 'ds-account-list',
@@ -7,6 +8,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./account-list.component.scss']
 })
 export class AccountListComponent implements OnInit {
+
+  transactions: ITransaction[] = [
+    {
+      date: '9 FEB 18',
+      accFrom: 1,
+      accTo: 2,
+      amount: 5000.00,
+      description: 'GIC principal cancelled from 101213153'
+    },
+    {
+      date: '1 FEB 18',
+      accFrom: 1,
+      accTo: 2,
+      amount: 345.00,
+      description: 'GIC principal cancelled from 101213154'
+    },
+    {
+      date: '31 JAN 18',
+      accFrom: 2,
+      accTo: 1,
+      amount: 97.05,
+      description: 'Interest received'
+    },
+    {
+      date: '5 JAN 18',
+      accFrom: 1,
+      accTo: 2,
+      amount: 150.00,
+      description: 'GIC purchase to 101213153'
+    },
+    {
+      date: '17 DIC 17',
+      accFrom: 2,
+      accTo: 1,
+      amount: 100.00,
+      description: 'GIC principal cancelled from 101213153'
+    }
+  ];
   
   constructor(){ }
 
